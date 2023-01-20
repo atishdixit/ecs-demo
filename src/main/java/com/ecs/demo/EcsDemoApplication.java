23fa7a5f7d67a67e4941835cbf553773883cf66e
atishdixit <atishdixit26@gmail.com>
2023-01-20 13:22:49 +0530

    Added new endpoint

--- src/main/java/com/ecs/demo/EcsDemoApplication.java
+++ src/main/java/com/ecs/demo/EcsDemoApplication.java
@@ -2,12 +2,19 @@ package com.ecs.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EcsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcsDemoApplication.class, args);
 	}
 
+	@GetMapping("/hello-world")
+	public String helloWorld(){
+		return "Hello World";
+	}
 }
